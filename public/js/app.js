f68a9b0438dc255777cd325ce1dffd0476764dce
unknown <traian.baciu87@gmail.com>
2025-08-11 14:16:59 +0300

    setup app for Render

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://localhost:3000/weather?address=";
+const baseUrl = "/weather?address=";
 
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
